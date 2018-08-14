85f7f1ad566ece557554a3e6a744eacf1372c478
tangsheng <ts@posun.cn>
2018-08-14 10:00:43 +0800

    更新博客

--- post/.vuepress/config.js
+++ post/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: "YupaiTS的博客",
     description: "个人技术博客",
-    base: "/blog/",
+    base: "/",
     head: [
         ['link', { rel: 'icon', href: '/logo.jpg' }] //设置favicon
     ],
@@ -10,7 +10,7 @@ module.exports = {
         displayAllHeaders: false, //显示所有页面的标题链接，默认只展开显示当前页面的标题链接
         activeHeaderLinks: true, //活动的标题链接，可随鼠标上下滑动自动激活标题侧边栏标题高亮，需要markdown.anchor.permalink: true
         lastUpdated: '上次更新',
-        docsRepo: 'https://github.com/YupaiTS/blog',
+        docsRepo: 'https://github.com/YupaiTS/yupaits.github.io',
         docsDir: 'post',
         docsBranch: 'master',
         editLinks: true,
