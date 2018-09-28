474198a7422a6a45153f0eef901794cf33fd42f2
tangsheng <ts@posun.cn>
2018-09-28 15:28:17 +0800

    测试travis-ci构建并部署博客

--- post/.vuepress/config.js
+++ post/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: '/logo.jpg' }] //设置favicon
     ],
-    dest: "docs",
+    dest: "dist",
     themeConfig: {
         displayAllHeaders: false, //显示所有页面的标题链接，默认只展开显示当前页面的标题链接
         activeHeaderLinks: true, //活动的标题链接，可随鼠标上下滑动自动激活标题侧边栏标题高亮，需要markdown.anchor.permalink: true
