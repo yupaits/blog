a4884f4b75055eb088cd7da4867ec47fa86804da
tangsheng <ts@posun.cn>
2019-08-20 14:37:55 +0800

    使用Gitalk实现博客评论功能

--- post/.vuepress/enhanceApp.js
+++ post/.vuepress/enhanceApp.js
@@ -5,4 +5,58 @@ export default ({
     siteData // 站点元数据
 }) => {
     // ...做一些其他的应用级别的优化
+    try {
+        document && useGitalk(router);
+    } catch (e) {
+        console.error(e.message);
+    }
+}
+
+function useGitalk(router) {
+    const linkGitalk = document.createElement('link');
+    linkGitalk.href = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css';
+    linkGitalk.rel = 'stylesheet';
+    document.body.append(linkGitalk);
+    const scriptGitalk = document.createElement('script');
+    scriptGitalk.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js';
+    document.body.appendChild(scriptGitalk);
+
+    router.afterEach((to) => {
+        if (scriptGitalk.onload) {
+            loadGitalk(to);
+        } else {
+            scriptGitalk.onload = () => {
+                loadGitalk(to);
+            }
+        }
+    });
+
+    function loadGitalk(to) {
+        let commentsContainer = document.getElementById('gitalk-container');
+        if (!commentsContainer) {
+            commentsContainer = document.createElement('div');
+            commentsContainer.id = 'gitalk-container';
+            commentsContainer.classList.add('content');
+        }
+        const $page = document.querySelector('.page');
+        if ($page) {
+            $page.appendChild(commentsContainer);
+            if (typeof Gitalk !== 'undefined' && Gitalk instanceof Function) {
+                renderGitalk(to.fullPath);
+            }
+        }
+    }
+
+    function renderGitalk(fullPath) {
+        const gitalk = new Gitalk({
+            clientID: '5680169c83c7f32a7352',
+            clientSecret: '1a71f599143d186c5e8fd00f3f96f482d8ff71ea',
+            repo: 'blog',
+            owner: 'yupaits',
+            admin: ['yupaits'],
+            distractionFreeMode: false,
+            language: 'zh-CN'
+        });
+        gitalk.render('gitalk-container');
+    }
 }
