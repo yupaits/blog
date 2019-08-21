8061dc4a3c15b02cfc026fc0d0dad18581f78258
tangsheng <ts@posun.cn>
2019-08-21 09:14:31 +0800

    增加gitalk忽略页面配置

--- builds/delMarkdown.js
+++ builds/delMarkdown.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 const findMarkdown = require('./findMarkdown')
 const rootDir = './post'
 
-findMarkdown(rootDir,delComponents)
+findMarkdown(rootDir, delComponents)
 
-function delComponents(dir){
-    fs.readFile(dir,'utf-8', (err, content) => {
-        if (err) throw err
+function delComponents(dir) {
+  fs.readFile(dir, 'utf-8', (err, content) => {
+    if (err) throw err
 
-        fs.writeFile(dir, content.replace(/\n \n <comment\/> \n /g,''), (err) => {
-            if (err) throw err
-            console.log(`del components from ${dir}`)
-          })
-      })
+    fs.writeFile(dir, content.replace(/\n \n <comment\/> \n /g, ''), (err) => {
+      if (err) throw err
+      console.log(`del components from ${dir}`)
+    })
+  })
 }
