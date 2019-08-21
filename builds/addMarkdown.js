2a5874bdd977fffefe5cb776e9e60400fe082d97
tangsheng <ts@posun.cn>
2019-08-21 09:22:53 +0800

    修正gitalk忽略页面文件判断逻辑

--- builds/addMarkdown.js
+++ builds/addMarkdown.js
@@ -6,7 +6,7 @@ const excludeFiles = ['./post/README.md']
 findMarkdown(rootDir, writeComponents)
 
 function writeComponents(dir) {
-  if (excludeFiles.indexOf(dir) !== -1) {
+  if (excludeFiles.indexOf(dir) === -1) {
     fs.appendFile(dir, `\n \n <comment/> \n `, (err) => {
       if (err) throw err
       console.log(`add components to ${dir}`)
