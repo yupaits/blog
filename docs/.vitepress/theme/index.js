a45c4b363b2afd8e48d242e162e2767ad0aa955a
yupaits <ts495606653@hotmail.com>
2024-02-29 15:56:19 +0800

    完成图片缩放功能

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,27 @@
 import DefaultTheme from 'vitepress/theme'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
 import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+
 import 'vitepress-plugin-back-to-top/dist/style.css'
+import './index.css'
 
 export default {
   ...DefaultTheme,
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      mediumZoom('.main img', { background: 'var(--c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+  },
   enhanceApp({ app }) {
     vitepressBackToTop({
       threshold: 300
