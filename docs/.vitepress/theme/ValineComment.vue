02444c330c3a228d57f461a67a2d9b539858cc67
yupaits <ts495606653@hotmail.com>
2024-03-03 15:04:42 +0800

    完成Valine评论系统整合

---
+++ docs/.vitepress/theme/ValineComment.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="comment">
+    <div class="visitors">
+      <span class="page-title">{{page.title ? `《${page.title}》` : null}}</span>
+      <span class="leancloud-visitors">
+        <span class="count-text">阅读量：</span>
+        <i class="leancloud-visitors-count"></i>
+      </span>
+    </div>
+    <div class="comment-rule">
+      <h4>评论区留言准则：</h4>
+      <p>1. 本评论区禁止传播封建迷信、吸烟酗酒、低俗色情、赌博诈骗等任何违法违规内容。</p>
+      <p>2. 当他人以不正当方式诱导打赏、私下交易，请谨慎判断，以防人身财产损失。</p>
+      <p>3. 请勿轻信各类招聘征婚、代练代抽、私下交易、购买礼包码、游戏币等广告信息，谨防网络诈骗。</p>
+    </div>
+    <div id="vcomments"></div>
+  </div>
+</template>
+
+<script setup>
+import { useData } from 'vitepress'
+
+const { page } = useData()
+</script>
+
+<style>
+.comment {
+  margin-top: 2rem;
+}
+
+.visitors {
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid var(--vp-c-divider);
+  border-radius: 8px;
+  padding: 8px 16px;
+  color: var(--vp-c-text-1);
+  line-height: 24px;
+  transition: color 0.25s;
+}
+
+.count-text {
+  font-size: 14px;
+  text-align: right;
+  color: var(--vp-c-text-2);
+}
+
+.comment-rule {
+  margin-top: 1rem;
+  border: 1px solid var(--vp-c-divider);
+  border-radius: 8px;
+  padding: 8px 16px;
+  color: var(--vp-c-text-2);
+  flex-grow: 1;
+  line-height: 24px;
+  font-size: 14px;
+  transition: color 0.25s;
+}
+
+#vcomments {
+  margin-top: 1rem;
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,23 +3,63 @@ import { onMounted, watch, nextTick } from 'vue'
 import { useRoute } from 'vitepress'
 import mediumZoom from 'medium-zoom'
 import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+import Valine from 'valine'
+import MyLayout from './MyLayout.vue'
 
 import 'vitepress-plugin-back-to-top/dist/style.css'
 import './index.css'
 
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
+  Layout: MyLayout,
   setup() {
     const route = useRoute()
+
+    // 初始化Valine
+    const initValine = () => {
+      let path = location.origin + location.pathname
+      document.getElementsByClassName("leancloud-visitors")[0].id = path
+      new Valine({
+        el: '#vcomments',
+        appId: '1bm9HBoZJKiYc9SaRKBlDfJy-gzGzoHsz',
+        appKey: 'EIF8JdXxwwF5PoT8mgeeHzqH',
+        path: path,
+        visitor: true,
+        lang: 'zh-CN',
+        enableQQ: true,
+        requiredFields: ['nick', 'mail'],
+        placeholder: '在上面填写完昵称和邮箱之后，请在这里写下您的留言\n昵称填写为QQ号时会自动获取QQ头像',
+      })
+    }
+
+    const remoteImport = (url) => {
+      return new Promise((resolve) => {
+        var head = document.getElementsByTagName('head')[0]
+        var script = document.createElement('script')
+        script.setAttribute('type', 'text/javascript')
+        script.setAttribute('src', url)
+        head.append(script)
+
+        script.onload = () => resolve()
+      })
+    }
+
+    // 图片缩放
     const initZoom = () => {
       mediumZoom('.main img', { background: 'rgba(0, 0, 0, 0.5)' })
     }
+
     onMounted(() => {
       initZoom()
+      remoteImport('//unpkg.com/valine/dist/Valine.min.js').then(() => initValine())
     })
+
     watch(
       () => route.path,
-      () => nextTick(() => initZoom())
+      () => {
+        initValine()
+        nextTick(() => initZoom())
+      }
     )
   },
   enhanceApp({ app }) {
