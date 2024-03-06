7432f42b9132d0196128e0fb11a879e1dd3e432f
yupaits <ts495606653@hotmail.com>
2024-03-06 20:38:46 +0800

    新增BookCardGroup组件

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,6 +5,7 @@ import mediumZoom from 'medium-zoom'
 import vitepressBackToTop from 'vitepress-plugin-back-to-top'
 import MyLayout from './MyLayout.vue'
 import DraftLayout from './layout/DraftLayout.vue'
+import BookCardGroup from './components/BookCardGroup.vue'
 import LinkCardGroup from './components/LinkCardGroup.vue'
 import Progress from './components/Progress.vue'
 
@@ -70,6 +71,7 @@ export default {
   },
   enhanceApp({ app }) {
     app.component('DraftLayout', DraftLayout)
+    app.component('BookCardGroup', BookCardGroup)
     app.component('LinkCardGroup', LinkCardGroup)
     app.component('Progress', Progress)
     vitepressBackToTop({
