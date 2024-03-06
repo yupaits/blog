cbb7b6e9d5f187a11ac85845831746eeaaa38f5d
yupaits <ts495606653@hotmail.com>
2024-03-06 10:26:57 +0800

    交互元素文案调整

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -642,9 +642,12 @@ export default defineConfig({
       next: '下一页'
     },
 
-    darkModeSwitchLabel: '暗黑模式',
-    sidebarMenuLabel: '文章列表',
+    langMenuLabel: '多语言',
     returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '文章列表',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
     externalLinkIcon: true,
   },
   markdown: {
