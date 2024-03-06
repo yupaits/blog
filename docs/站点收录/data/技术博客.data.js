dc3cd5d7b78f04ca209bd65db267e4084703a9bb
yupaits <ts495606653@hotmail.com>
2024-03-06 19:54:06 +0800

    技术博客使用LinkCardGroup组件

---
+++ docs/站点收录/data/技术博客.data.js
@@ -0,0 +1,31 @@
+export default {
+  load() {
+    return [
+      {
+        text: '研发',
+        items: [
+          { text: '美团技术团队', link: 'https://tech.meituan.com/', icon: 'https://awps-assets.meituan.net/mit/blog/v20190629/asset/icon/android-icon-192x192.png?v=Whistle&t=20181017-1r' },
+          { text: '有赞技术团队', link: 'https://tech.youzan.com/' },
+          { text: '阮一峰的个人网站', link: 'http://www.ruanyifeng.com/' },
+          { text: '廖雪峰的官方网站', link: 'https://www.liaoxuefeng.com/' },
+          { text: 'JavaGuide', link: 'https://javaguide.cn/' },
+          { text: 'advanced-java', link: 'https://doocs.github.io/advanced-java/', icon: 'https://doocs.github.io/advanced-java/images/favicon-32x32.png' },
+          { text: '程序员DD', link: 'http://blog.didispace.com/', icon: 'https://www.didispace.com/logo.png' },
+          { text: '周立的博客', link: 'https://www.itmuch.com/' },
+          { text: "HollisChuang's Blog", link: 'http://www.hollischuang.com/' },
+          { text: '芋道源码', link: 'http://www.iocoder.cn/', icon: 'https://www.iocoder.cn/images/favicon.ico' },
+          { text: '萌叔', link: 'http://vearne.cc/' },
+          { text: "crossoverJie's Blog", link: 'http://crossoverjie.top/' },
+        ]
+      },
+      {
+        text: '安全',
+        items: [
+          { text: '看雪学苑', link: 'https://www.kanxue.com/', icon: '/icon/url.png' },
+          { text: '知道创宇Paper', link: 'https://paper.seebug.org/', icon: 'https://paper.seebug.org/static/images/favicon.ico' },
+          { text: '微慑信息网', link: 'https://vulsee.com/' },
+        ]
+      }
+    ]
+  }
+}
