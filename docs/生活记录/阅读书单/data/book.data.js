d678367cc35936f4b15aace424cd95ba8168c20e
yupaits <ts495606653@hotmail.com>
2024-03-07 20:22:21 +0800

    《阅读计划》使用LinkCardGroup组件

---
+++ docs/生活记录/阅读书单/data/book.data.js
@@ -0,0 +1,74 @@
+export default {
+  load() {
+    return [
+      {
+        text: '科幻',
+        imgWidth: '64px',
+        imgHeight: '80px',
+        items: [
+          {
+            icon: "./covers/三体全集.webp",
+            text: "三体全集",
+            link: "https://book.douban.com/subject/30306441/",
+            description: '刘慈欣'
+          }
+        ]
+      },
+      {
+        text: '推理悬疑',
+        imgWidth: '64px',
+        imgHeight: '80px',
+        items: [
+          {
+            icon: "./covers/无人生还.webp",
+            text: "无人生还",
+            link: "https://book.douban.com/subject/24859822/",
+            description: "[英] 阿加莎.克里斯蒂"
+          },
+          {
+            icon: "./covers/悬崖上的谋杀.webp",
+            text: "悬崖上的谋杀",
+            link: "https://book.douban.com/subject/33400116/",
+            description: "[英] 阿加莎.克里斯蒂"
+          },
+          {
+            icon: "./covers/奉命谋杀.webp",
+            text: "奉命谋杀",
+            link: "https://book.douban.com/subject/27063586/",
+            description: "[英] 阿加莎.克里斯蒂"
+          },
+          {
+            icon: "./covers/长夜.webp",
+            text: "长夜",
+            link: "https://book.douban.com/subject/25844726/",
+            description: "[英] 阿加莎.克里斯蒂"
+          },
+          {
+            icon: "./covers/解忧杂货铺.webp",
+            text: "解忧杂货铺",
+            link: "https://book.douban.com/subject/25862578/",
+            description: "[日] 东野圭吾"
+          },
+          {
+            icon: "./covers/刺杀骑士团长.webp",
+            text: "刺杀骑士团长",
+            link: "https://book.douban.com/subject/27199470/",
+            description: "[日] 村上春树"
+          },
+          {
+            icon: "./covers/白夜行.webp",
+            text: "白夜行",
+            link: "https://book.douban.com/subject/10554308/",
+            description: "[日] 东野圭吾"
+          },
+          {
+            icon: "./covers/虚无的十字架.webp",
+            text: "虚无的十字架",
+            link: "https://book.douban.com/subject/26413027/",
+            description: "[日] 东野圭吾"
+          },
+        ]
+      }
+    ]
+  }
+}
