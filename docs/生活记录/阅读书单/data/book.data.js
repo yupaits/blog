ab3697657f97bcd02eee3939af89e198402f0d5e
yupaits <ts495606653@hotmail.com>
2024-03-08 09:49:38 +0800

    阅读书单书籍信息源更换为微信读书

--- docs/生活记录/阅读书单/data/book.data.js
+++ docs/生活记录/阅读书单/data/book.data.js
@@ -7,9 +7,9 @@ export default {
         imgHeight: '80px',
         items: [
           {
-            icon: "./covers/三体全集.webp",
+            icon: "https://cdn.weread.qq.com/weread/cover/80/yuewen_695233/t6_yuewen_6952331677562148.jpg",
             text: "三体全集",
-            link: "https://book.douban.com/subject/30306441/",
+            link: "https://weread.qq.com/web/bookDetail/ce032b305a9bc1ce0b0dd2a",
             description: '刘慈欣'
           }
         ]
@@ -20,51 +20,52 @@ export default {
         imgHeight: '80px',
         items: [
           {
-            icon: "./covers/无人生还.webp",
+            icon: "https://cdn.weread.qq.com/weread/cover/63/yuewen_728514/t6_yuewen_7285141680854106.jpg",
+            // icon: "/book_covers/无人生还.webp",
             text: "无人生还",
-            link: "https://book.douban.com/subject/24859822/",
+            link: "https://weread.qq.com/web/bookDetail/d6332fd05b1dc2d63df8938",
             description: "[英] 阿加莎.克里斯蒂"
           },
           {
-            icon: "./covers/悬崖上的谋杀.webp",
+            icon: "https://cdn.weread.qq.com/weread/cover/32/YueWen_25566754/t6_YueWen_25566754.jpg",
             text: "悬崖上的谋杀",
-            link: "https://book.douban.com/subject/33400116/",
+            link: "https://weread.qq.com/web/bookDetail/bf43221071861e22bf42c79",
             description: "[英] 阿加莎.克里斯蒂"
           },
           {
-            icon: "./covers/奉命谋杀.webp",
+            icon: "https://cdn.weread.qq.com/weread/cover/82/YueWen_22289889/t6_YueWen_22289889.jpg",
             text: "奉命谋杀",
-            link: "https://book.douban.com/subject/27063586/",
+            link: "https://weread.qq.com/web/bookDetail/87b3207071541de187b4895",
             description: "[英] 阿加莎.克里斯蒂"
           },
           {
-            icon: "./covers/长夜.webp",
+            icon: "https://wfqqreader-1252317822.image.myqcloud.com/cover/526/728526/t6_728526.jpg",
             text: "长夜",
-            link: "https://book.douban.com/subject/25844726/",
+            link: "https://weread.qq.com/web/bookDetail/089326905b1dce08935624b",
             description: "[英] 阿加莎.克里斯蒂"
           },
           {
-            icon: "./covers/解忧杂货铺.webp",
-            text: "解忧杂货铺",
-            link: "https://book.douban.com/subject/25862578/",
+            icon: "https://cdn.weread.qq.com/weread/cover/27/3300020527/t6_3300020527.jpg",
+            text: "解忧杂货店",
+            link: "https://weread.qq.com/web/bookDetail/6d132250813ab6e84g017ca5",
             description: "[日] 东野圭吾"
           },
           {
-            icon: "./covers/刺杀骑士团长.webp",
+            icon: "https://cdn.weread.qq.com/weread/cover/12/cpplatform_27ztfi8zjc17r4ruzuxfws/t6_cpplatform_27ztfi8zjc17r4ruzuxfws1686221000.jpg",
             text: "刺杀骑士团长",
-            link: "https://book.douban.com/subject/27199470/",
+            link: "https://weread.qq.com/web/bookDetail/3a832bc0813ab7e67g0178e2",
             description: "[日] 村上春树"
           },
           {
-            icon: "./covers/白夜行.webp",
+            icon: "https://cdn.weread.qq.com/weread/cover/1/yuewen_230107/t6_yuewen_2301071682244014.jpg",
             text: "白夜行",
-            link: "https://book.douban.com/subject/10554308/",
+            link: "https://weread.qq.com/web/bookDetail/65032c105382db65050e7aa",
             description: "[日] 东野圭吾"
           },
           {
-            icon: "./covers/虚无的十字架.webp",
+            icon: "https://cdn.weread.qq.com/weread/cover/22/cpPlatform_kboEK7t9eX3aznMwkW7YVz/t6_cpPlatform_kboEK7t9eX3aznMwkW7YVz.jpg",
             text: "虚无的十字架",
-            link: "https://book.douban.com/subject/26413027/",
+            link: "https://weread.qq.com/web/bookDetail/dd832e80813ab74b7g017596",
             description: "[日] 东野圭吾"
           },
         ]
