ce4856a959ef422ce5fccca3a6a92602d4278ac6
yupaits <ts495606653@hotmail.com>
2024-09-21 11:27:53 +0800

    ElTimelineItem组件样式导入

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -12,6 +12,7 @@ import { ElTimeline, ElTimelineItem, ElWatermark } from 'element-plus'
 
 import 'vitepress-plugin-back-to-top/dist/style.css'
 import 'element-plus/es/components/timeline/style/css'
+import 'element-plus/es/components/timeline-item/style/css'
 import 'element-plus/es/components/watermark/style/css'
 import './style/index.css'
 import './style/var.css'
