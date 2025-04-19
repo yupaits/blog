9ccdf94b88cccdb70e8247be5a9798d5387850f0
yupaits <ts495606653@hotmail.com>
2025-04-19 22:52:25 +0800

    页面加载添加动画效果

--- docs/.vitepress/theme/components/BlogPage.vue
+++ docs/.vitepress/theme/components/BlogPage.vue
@@ -1,5 +1,9 @@
 <template>
   <Layout>
+    <template #doc-top>
+      <div class="shade" :class="{'shade-active': isTransitioning}"></div>
+    </template>
+
     <template #doc-before>
       <DraftAnnounce :preview="isPreviewDraft()" :percent="frontmatter?.draftPercent" v-if="isDraft()" />
     </template>
@@ -11,7 +15,7 @@
 </template>
 
 <script setup>
-import { onMounted, watch } from 'vue'
+import { onMounted, watch, ref } from 'vue'
 import { useData, useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import DraftAnnounce from './DraftAnnounce.vue'
@@ -19,6 +23,7 @@ import ValineComment from './ValineComment.vue'
 const { Layout } = DefaultTheme
 const { frontmatter } = useData()
 const route = useRoute()
+const isTransitioning = ref(false)
 
 const isDraft = () => {
   const isDraftStatus = frontmatter.value?.draft
@@ -43,12 +48,55 @@ const showComment = () => {
     (!isDraft() || isPreviewDraft())
 }
 
+const transition = () => {
+  isTransitioning.value = true
+  setTimeout(() => {
+    isTransitioning.value = false
+  }, 300)
+}
+
 onMounted(() => {
+  transition()
   handlePreviewDraft()
 })
 
 watch(
   () => route.path,
-  () => handlePreviewDraft()
+  () => {
+    transition()
+    handlePreviewDraft()
+  }
 )
 </script>
+
+<style>
+.shade {
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(255, 255, 255);
+  z-index: 100;
+  pointer-events: none;
+  opacity: 0;
+  transition: transform 0.3s ease-in-out;
+}
+
+.shade-active {
+  opacity: 0;
+  animation: shadeAnimation 0.3s ease-in-out;
+}
+
+@keyframes shadeAnimation {
+  0% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(100vh);
+  }
+}
+</style>
