def93f0a683a65cec92de7ab9c234c0792c4c40b
yupaits <ts495606653@hotmail.com>
2026-02-10 11:14:06 +0800

    AI生产力数据更新

--- docs/站点收录/data/AI生产力.data.js
+++ docs/站点收录/data/AI生产力.data.js
@@ -5,19 +5,21 @@ export default {
         text: 'AI助手',
         items: [
           { text: 'DeepSeek', link: 'https://chat.deepseek.com/', icon: 'https://cdn.deepseek.com/chat/icon.png' },
-          { text: '豆包（字节跳动）', link: 'https://www.doubao.com/chat/', icon: 'https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/doubao/web/logo-icon.png' },
-          { text: '通义（阿里巴巴）', link: 'https://www.tongyi.com/', icon: 'https://img.alicdn.com/imgextra/i4/O1CN01EfJVFQ1uZPd7W4W6i_!!6000000006051-2-tps-112-112.png' },
-          { text: '腾讯元宝', link: 'https://yuanbao.tencent.com/chat', icon: 'https://cdn-bot.hunyuan.tencent.com/logo-v2.png' },
+          { text: '豆包', link: 'https://www.doubao.com/chat/', icon: 'https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/doubao/web/logo-icon.png' },
+          { text: '千问', link: 'https://www.tongyi.com/', icon: 'https://img.alicdn.com/imgextra/i4/O1CN01uar8u91DHWktnF2fl_!!6000000000191-2-tps-110-110.png' },
+          { text: '元宝', link: 'https://yuanbao.tencent.com/chat', icon: 'https://cdn-bot.hunyuan.tencent.com/logo-v2.png' },
           { text: 'Kimi', link: 'https://kimi.moonshot.cn/', icon: 'https://statics.moonshot.cn/kimi-web-seo/assets/favicon-OXkYivQY.ico' },
-          { text: '文心一言（百度）', link: 'https://yiyan.baidu.com/', icon: 'https://nlp-eb.cdn.bcebos.com/logo/favicon.ico' },
+          { text: '文心一言', link: 'https://yiyan.baidu.com/', icon: 'https://nlp-eb.cdn.bcebos.com/logo/favicon.ico' },
+          { text: '即梦AI', link: 'https://jimeng.jianying.com/', icon: 'https://sf16-web-tos-buz.capcutstatic.com/obj/capcut-web-buz-sg/common/images/dreamina-v3.ico' },
         ]
       },
       {
         text: '专业领域',
         items: [
-          { text: 'Trae（字节跳动）', description: '一个能理解需求、调动工具、独立完成各类开发任务的超级AI开发工程师', link: 'https://www.trae.com.cn/', icon: 'https://lf-cdn.trae.com.cn/obj/trae-com-cn/trae_website_prod_cn/favicon.png' },
-          { text: 'CodeBuddyIDE（腾讯）', description: 'AI 一站式工作台：从此用自然语言实现产品、设计、研发全流程', link: 'https://copilot.tencent.com/ide', icon: 'https://codebuddy-1328495429.cos.accelerate.myqcloud.com/web/ide/icon.png' },
-          { text: 'ARC（腾讯）', description: '探索挑战智能媒体相关前沿技术', link: 'https://arc.tencent.com/zh/index', icon: 'https://arc.tencent.com/logo.ico' },
+          { text: 'Trae', description: '一个能理解需求、调动工具、独立完成各类开发任务的超级AI开发工程师', link: 'https://www.trae.com.cn/', icon: 'https://lf-cdn.trae.com.cn/obj/trae-com-cn/trae_website_prod_cn/favicon.png' },
+          { text: 'CodeBuddyIDE', description: 'AI 一站式工作台：从此用自然语言实现产品、设计、研发全流程', link: 'https://copilot.tencent.com/ide', icon: 'https://codebuddy-1328495429.cos.accelerate.myqcloud.com/web/ide/icon.png' },
+          { text: 'ARC', description: '探索挑战智能媒体相关前沿技术', link: 'https://arc.tencent.com/zh/index', icon: 'https://arc.tencent.com/logo.ico' },
+          { text: '蚂蚁阿福', description: '健康是福，健康的事就找阿福', link: 'https://www.antafu.com/', icon: 'https://mdn.alipayobjects.com/rms/afts/file/A*1LduTae8R5sAAAAAQaAAAAgAehQnAQ' },
         ]
       },
       {
