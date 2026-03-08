14b221c6470309afaaae62ec1760d36648c37904
yupaits <ts495606653@hotmail.com>
2026-03-08 11:49:58 +0800

    更新个人技能信息

--- docs/技术博客/data/技能.data.js
+++ docs/技术博客/data/技能.data.js
@@ -1,18 +1,18 @@
 export default {
   load() {
     return [
-      { text: 'Java', percent: 80 },
-      { text: 'Spring Boot', percent: 70 },
+      { text: 'Java', percent: 85 },
+      { text: 'Spring Boot', percent: 80 },
+      { text: 'MySQL', percent: 80 },
+      { text: 'Maven', percent: 75 },
       { text: 'Spring Cloud', percent: 70 },
-      { text: 'MySQL', percent: 70 },
-      { text: 'Maven', percent: 60 },
-      { text: 'Vue.js', percent: 60 },
-      { text: 'React', percent: 50 },
+      { text: 'Javascript', percent: 68 },
+      { text: 'Vue.js', percent: 65 },
+      { text: 'React', percent: 60 },
       { text: 'Git', percent: 60 },
       { text: 'Docker', percent: 50 },
       { text: 'CI/CD', percent: 50 },
       { text: 'Golang', percent: 50 },
-      { text: 'Javascript', percent: 50 },
     ]
   }
 }
