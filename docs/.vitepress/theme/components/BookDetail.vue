4744661945a69e5f5077fa8c3b674988e169b7f5
yupaits <ts495606653@hotmail.com>
2026-05-20 19:06:27 +0800

    BookDetail组件添加tag展示

--- docs/.vitepress/theme/components/BookDetail.vue
+++ docs/.vitepress/theme/components/BookDetail.vue
@@ -1,6 +1,7 @@
 <template>
   <span>
-    <span class="author">{{ author }}</span>
+    <span class="badge">{{ author }}</span>
+    <span class="badge badge-tag" v-if="tag">{{ tag }}</span>
     <br>
     <span>{{ intro }}</span>
   </span>
@@ -8,16 +9,21 @@
 
 <script setup>
 import { defineProps } from 'vue'
-const { author, intro } = defineProps(['author', 'intro'])
+const { author, tag, intro } = defineProps(['author', 'tag', 'intro'])
 </script>
 
 <style scoped>
-.author {
+.badge {
   cursor: default;
   color: var(--vp-c-text-2);
   border: 1px solid var(--vp-c-divider);
   border-radius: 4px;
   background-color: var(--vp-c-brand-soft);
   padding: 0 6px;
+}
+
+.badge-tag {
+  margin-left: 8px;
+  background-color: var(--vp-badge-info-bg);
 }
 </style>
